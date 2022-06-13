0f835ebcbb33e089e400976fa15e050df7d7dd40
Kondwani Kamundi <kkamundi@gmail.com>
2022-06-13 11:45:07 +0200

    added coops data

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
+import time
+
+# motor
+import RPi.GPIO as GPIO
+import flask
 from flask import Flask
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import current_user, login_user, logout_user, LoginManager
 from flask_login import login_required
-
 from flask_migrate import Migrate
-
-from datetime import datetime
-
 from werkzeug.security import generate_password_hash, check_password_hash
-#motor
-import RPi.GPIO as GPIO
-import time
 
 GPIO.setmode(GPIO.BOARD)
 
-from .models import  db, Users, Recipe, Coops
-from .schema import RecipeSchema, ProfileSchema, CoopsSchema, ma
+from .models import db, Users, Recipe, Coops
+from .schema import RecipeSchema, CoopsSchema, ma
 
 app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///easymixdb.sqlite'
@@ -35,47 +33,53 @@ login_manager.login_view = '.login'
 def load_user(user_id):
     return Users.query.get(int(user_id))
 
+
 @app.route('/')
 def easyindex():
     return render_template('index.html')
 
+
 @app.route('/profile')
 @login_required
 def profile():
-     return render_template('profile.html',
-      username=current_user.username, id=current_user.id, phone=current_user.phone, email=current_user.email
-      )
-      
-      
+    return render_template('profile.html',
+                           username=current_user.username, id=current_user.id, phone=current_user.phone,
+                           email=current_user.email
+                           )
+
+
 @app.route('/updatemyprofile')
 @login_required
 def updprofile():
-     return render_template('updatemyprofile.html'
-      )
-@app.route('/myprofile/update/<int:id>/', methods=['GET','POST'])
+    return render_template('updatemyprofile.html'
+                           )
+
+
+@app.route('/myprofile/update/<int:id>/', methods=['GET', 'POST'])
 @login_required
 def update_myprofile(id):
-    #form = User
-    #form = UserForm
- if request.method == "POST":
-    name_to_update = Users.query.get_or_404('id')
-   
-    name_to_update.username = request.form['username']
-    name_to_update.email= request.form['email']
-    name_to_update.username = request.form['phone']
-   #UPDATE PASSWORD
-    
-    db.session.commit()
-    flash("User updated successfully")
-    #return redirect("updatemyprofile.html")
-    return redirect(url_for('updatemyprofile'))
+    # form = User
+    # form = UserForm
+    if request.method == "POST":
+        name_to_update = Users.query.get_or_404('id')
+
+        name_to_update.username = request.form['username']
+        name_to_update.email = request.form['email']
+        name_to_update.username = request.form['phone']
+        # UPDATE PASSWORD
+
+        db.session.commit()
+        flash("User updated successfully")
+        # return redirect("updatemyprofile.html")
+        return redirect(url_for('updatemyprofile'))
+
 
 @app.route('/displaycoop')
 @login_required
 def display_coop():
     return render_template('displaycoop.html')
 
-    
+
 @app.route('/login')
 def login():
     return render_template('login.html')
@@ -98,92 +102,194 @@ def login_post():
     login_user(user, remember=remember)
     return redirect('/farmer_dashboard')
 
+
 @app.route('/add_coop')
 @login_required
 def add_coop():
     return render_template('add_coop.html')
-    
+
+
 @app.route('/Easymix_previewmycoop')
 @login_required
 def preview_coop():
     return render_template('Easymix_previewmycoop.html')
-    
+
+
 @app.route('/Easymix_viewmycoop')
 @login_required
 def viewmycoop():
     return render_template('Easymix_viewmycoop.html')
 
 
-
 @app.route('/coop_adding', methods=['POST', 'GET'])
 def addcoop_post():
-        
-        name = request.form.get('name')
-        age = request.form.get('age')
-        breed = request.form.get('breed')
-        number = request.form.get('number')
-        new_coop = Coops(name=name, breed=breed, age=age, number=number, user_id=current_user.id)
-    
-        #push to database
-        
-        db.session.add(new_coop)
-        db.session.commit()
-        flash('Coop added succesfully')
-        return redirect('/add_coop')   
-   
-#     coop = Coops.query.filter_by(
-#         name=name).first()
-#     
-#     if coop:
-#         flash('The chicken house with this name already exists')
-#         return redirect('/farmer_dashboard')
+    # id  --> auto
+    # name  --> supplied
+    # age --> supplied
+    # breed  --> supplied
+    # number_of_days_to_feed --> supplied, in a range
+    # number_of_chicks  --> supplied
+    # date_for_next_feed --> auto
+    # date_added --> auto
+    # user_id --> auto
+    # recipe_object  --> auto
+
+    name = request.form.get('name')
+    # TODO : the farmer can only input numbers
+    age = int(request.form.get('age'))
+    breed = request.form.get('breed')
+    number_of_chickens = int(request.form.get('number'))
+    new_coop = Coops(name=name, breed=breed, age=age, number_of_chicks=number_of_chickens, user_id=current_user.id)
+    db.session.add(new_coop)
+    db.session.commit()
+    flash('Coop added successfully')
+    return redirect('/coop/add_days_to_feed/' + str(new_coop.id))
 
 
-@app.route('/coops/show', methods = ['POST', 'GET'])
+@app.route('/coop/add_days_to_feed/<int:id_>', methods=['GET', 'POST'])
+def add_days_coops(id_):
+    # id  --> auto
+    # name  --> supplied
+    # age --> supplied
+    # breed  --> supplied
+    # number_of_days_to_feed --> supplied, in a range
+    # number_of_chicks  --> supplied
+    # date_for_next_feed --> auto
+    # date_added --> auto
+    # user_id --> auto
+    # recipe_object  --> auto
+
+    coop_instance = Coops.query.filter_by(id=id_)
+    # logic to calculate available number of days to feed
+    available_days = 0
+    if coop_instance.breed == "layers":
+
+        if coop_instance.age >= 0 <= 28:
+            "chick mash"
+            available_days = 28 - coop_instance.age
+
+        elif coop_instance.ageage > 28 <= 119:
+            "growers mash"
+            available_days = 119 - coop_instance.age
+        else:
+            "layers mash"
+            available_days = 200
+
+    # get the available days for adding feed
+    if flask.request.method == 'GET':
+
+        return render_template('add_coop', data={"available_days": available_days})
+
+
+    else:
+        # validate if entered days are valid
+        number_of_days_to_feed = int(request.form.get('number_of_days_to_feed'))
+        if available_days == 200:
+            # we dont need to validate what the farmer entered
+            pass
+        else:
+            if available_days < number_of_days_to_feed:
+                # this is an error, the farmer has entered wrong value
+                flash('Please enter correct data')
+                return render_template('add_coop', data={"available_days": available_days})
+
+
+#     # the logic of calculating days for next feed & feed type
+#     if breed == "layers":
+#         daily_consumption = 110
+#         feed = ""
+#         wmaize = 0
+#         soya = 0
+#         fishm = 0
+#         maizeb = 0
+#         limes = 0
+#
+#         if age >= 0 <= 28:
+#             "chick mash"
+#             multiply_by = number_of_days_to_feed * number_of_chickens * daily_consumption
+#             wmaize = (0.5 * multiply_by)
+#             soya = (0.2 * multiply_by)
+#             fishm = (0.1 * multiply_by)
+#             maizeb = (0.1 * multiply_by)
+#             limes = (0.1 * multiply_by)
+#             result = wmaize + soya + fishm + maizeb + limes
+#             feed = "chick mash"
+#
+#
+#
+#         elif age > 28 <= 119:
+#             "growers mash"
+#             result = wmaize + soya + fishm + maizeb + limes
+#             feed = "growers mash"
+#
+#         else:
+#             "layers mash"
+#             result = wmaize + soya + fishm + maizeb + limes
+#             feed = "layers mash"
+#
+#         print(result)
+#         # saving the results into database
+#         recipe_instance = Recipe(feed=feed, number_of_days_to_feed=" ", result=result, user_id=current_user.id)
+#         db.session.add(recipe_instance)
+#         db.session.commit()
+#
+#     # push to database
+#
+#
+# #     coop = Coops.query.filter_by(
+# #         name=name).first()
+# #
+# #     if coop:
+# #         flash('The chicken house with this name already exists')
+# #         return redirect('/farmer_dashboard')
+
+
+@app.route('/coops/show', methods=['POST', 'GET'])
 def get_coop_data():
     coop_schema = CoopsSchema(many=True)
-    
+
     all_coops = Coops.query.filter_by(user_id=current_user.id).all()
     coop_data = coop_schema.dump(all_coops)
-    
+
     return render_template(
         'displaycoop.html',
         data=coop_data
-        )
-        
-@app.route('/mypreview', methods = ['POST', 'GET'])
+    )
+
+
+@app.route('/mypreview', methods=['POST', 'GET'])
 def get_mypreviewcoop_data():
     previewcoop_schema = CoopsSchema(many=True)
-    
+
     preview_coop = Coops.query.filter_by(user_id=current_user.id).all()
     mycoop_preview = previewcoop_schema.dump(preview_coop)
-    
-    return render_template(
-    'Easymix_previewmycoop.html',
-    data=mycoop_preview
-    )
-    
 
-@app.route('/viewmycoop', methods = ['POST', 'GET'])
-def get_viewmycoop_data():
-    viewcoop_schema = CoopsSchema(many=True)
-    
-#     view_coop = Coops.query.filter_by(user_id=current_user.id).all()
-#     mycoop_view = previewcoop_schema.dump(preview_coop)
-
-#Need to view only that specific coop for my current user ID ---KOndwani
-#then have calculate and cancel option 
-    
     return render_template(
-    'Easymix_viewmycoop.html',
-    data=mycoop_view
+        'Easymix_previewmycoop.html',
+        data=mycoop_preview
     )
 
+
+# @app.route('/viewmycoop', methods=['POST', 'GET'])
+# def get_viewmycoop_data():
+#     viewcoop_schema = CoopsSchema(many=True)
+#
+#     #     view_coop = Coops.query.filter_by(user_id=current_user.id).all()
+#     #     mycoop_view = previewcoop_schema.dump(preview_coop)
+#
+#     # Need to view only that specific coop for my current user ID ---KOndwani
+#     # then have calculate and cancel option
+#
+#     return render_template(
+#         'Easymix_viewmycoop.html',
+#         data=mycoop_view
+#     )
 
 
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
+
 
 @app.route('/signup', methods=['POST'])
 def signup_post():
@@ -200,7 +306,8 @@ def signup_post():
         return redirect('/signup')
 
     # creating a new user with the form data. Hash the password so the plaintext version isn't saved.
-    new_user = Users(email=email, phone=phone, username=name, password=generate_password_hash(password, method='sha256'))
+    new_user = Users(email=email, phone=phone, username=name,
+                     password=generate_password_hash(password, method='sha256'))
     flash('You have been added succesfully')
     # add the new user to the database
     db.session.add(new_user)
@@ -226,6 +333,7 @@ def layers():
 def layersall():
     return render_template('layers_all.html')
 
+
 # @app.route('/admin_dashboard')
 # @login_required
 # def admin_db():
@@ -240,15 +348,14 @@ def layersall():
 @app.route('/farmer_dashboard')
 @login_required
 def farmer_db():
-    
     return render_template('farmer_dashboard.html', username=current_user.username)
-    
 
 
 @app.route('/admin_dashboard')
 def admin_db():
     return render_template('/admin_dashboard.html')
-    
+
+
 # @app.route('/coop/createformula', methods=['POST','GET'])
 # @login_required
 # def coop_formula():
@@ -266,10 +373,10 @@ def admin_db():
 @login_required
 def getfeed_results():
     return render_template('feedresults.html')
-                           #id=current_coop.id)
+    # id=current_coop.id)
 
 
-@app.route('/feed/results', methods = ['POST','GET'])
+@app.route('/feed/results', methods=['POST', 'GET'])
 def feed_result():
     wmaize = ''
     soya = ''
@@ -277,32 +384,30 @@ def feed_result():
     maizeb = ''
     limes = ''
 
+    # calculate current age
+    days = request.form['days']
 
-    #calculate current age
-        days = request.form['days']
-        
-      
-       
 
-     if breed == "layers":
-         if age
-         daily_consumption ={{coop.number}}*150
-         total_consumption= daily_consumption*{{days to feed}}
-         wmaize = (0.5 * input1)
-         soya = (0.2 * input1)
-         fishm = (0.1 * input1)
-         maizeb = (0.1 * input1)
-         limes = (0.1 * input1)
-         return render_template('')
-        
-        if age
-        daily_consumption ={{coop.number}}*170
-        wmaize = (0.3 * input1)
-        soya = (0.2 * input1)
-        fishm = (0.2 * input1)
-        maizeb = (0.3 * input1)
-      
-
+# if breed == "layers":
+#     if age
+#         daily_consumption = {{coop.number}} * 150
+#     total_consumption = daily_consumption * {{days
+#     to
+#     feed}}
+#     wmaize = (0.5 * input1)
+#     soya = (0.2 * input1)
+#     fishm = (0.1 * input1)
+#     maizeb = (0.1 * input1)
+#     limes = (0.1 * input1)
+#     return render_template('')
+#
+# if age
+#     daily_consumption = {{coop.number}} * 170
+#     wmaize = (0.3 * input1)
+#     soya = (0.2 * input1)
+#     fishm = (0.2 * input1)
+#     maizeb = (0.3 * input1)
+#
 
 @app.route('/calc_result', methods=['POST', 'GET'])
 @login_required
@@ -331,7 +436,6 @@ def calc_result():  # ~ Route to send calculator form input
         recipe_instance = Recipe(feed=feed, amount_entered=amount_input, result=result, user_id=current_user.id)
         db.session.add(recipe_instance)
         db.session.commit()
-        
 
         return render_template(
             'layers1.html',
@@ -342,75 +446,74 @@ def calc_result():  # ~ Route to send calculator form input
             fishm=fishm,
             limes=limes,
             result=result)
-    
-            
+
+
 @app.route('/demo')
 def demo():
     return render_template('demo.html')
 
+
 @app.route('/demo_calc', methods=['POST', 'GET'])
 def demo_calc():
-    ufa='' #feed A
-    soya=''#feed B
-    
-    amount_input=request.form['total']
-    feed=request.form['feed']
-    input1=float(amount_input)
-    if feed =="demo":
-        ufa = (0.5*input1)     #ufa,soya,fishmeal answer will be stored in db, create a demo db with fields for these three
-        soya = (0.3*input1)
-        #fishmeal = (0.2*input1)
+    ufa = ''  # feed A
+    soya = ''  # feed B
 
-    motor1 = [7,11,13,15] #first motor pins defined
-    motor2 = [29,31,33,35]
-    
-    
-    #for motor 1 ufa
+    amount_input = request.form['total']
+    feed = request.form['feed']
+    input1 = float(amount_input)
+    if feed == "demo":
+        ufa = (
+                0.5 * input1)  # ufa,soya,fishmeal answer will be stored in db, create a demo db with fields for these three
+        soya = (0.3 * input1)
+        # fishmeal = (0.2*input1)
+
+    motor1 = [7, 11, 13, 15]  # first motor pins defined
+    motor2 = [29, 31, 33, 35]
+
+    # for motor 1 ufa
     for pin in motor1:
         GPIO.setup(pin, GPIO.OUT)
         GPIO.output(pin, 0)
     halfstep_seq = [
-      [1,0,0,0],
-      [1,1,0,0],
-      [0,1,0,0],
-      [0,1,1,0],
-      [0,0,1,0],
-      [0,0,1,1],
-      [0,0,0,1],
-      [1,0,0,1]
+        [1, 0, 0, 0],
+        [1, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 1],
+        [0, 0, 0, 1],
+        [1, 0, 0, 1]
     ]
-    
-    for i in range(int(512*ufa)): #rotating for feed A
-      for halfstep in range(8):
-        for pin in range(4):
-            GPIO.output(motor1[pin], halfstep_seq[halfstep][pin])
-            time.sleep(0.001)
-    #GPIO.cleanup()
-    
+
+    for i in range(int(512 * ufa)):  # rotating for feed A
+        for halfstep in range(8):
+            for pin in range(4):
+                GPIO.output(motor1[pin], halfstep_seq[halfstep][pin])
+                time.sleep(0.001)
+    # GPIO.cleanup()
+
     # for motor 1 end
-    
-    #motor 2
+
+    # motor 2
     for pin in motor2:
         GPIO.setup(pin, GPIO.OUT)
         GPIO.output(pin, 0)
 
-    
-    for i in range(int(512*soya)): #rotating for feed A
-      for halfstep in range(8):
-        for pin in range(4):
-            GPIO.output(motor2[pin], halfstep_seq[halfstep][pin])
-            time.sleep(0.001)
+    for i in range(int(512 * soya)):  # rotating for feed A
+        for halfstep in range(8):
+            for pin in range(4):
+                GPIO.output(motor2[pin], halfstep_seq[halfstep][pin])
+                time.sleep(0.001)
     GPIO.cleanup()
-    
-    #motor2 end 
-    
+
+    # motor2 end
+
     return render_template(
-            'demo_result.html',
-            input1=input1, feed=feed,
-            ufa=ufa, soya=soya)
-    
-    
-   
+        'demo_result.html',
+        input1=input1, feed=feed,
+        ufa=ufa, soya=soya)
+
+
 @app.route('/user/results', methods=['POST', 'GET'])
 @login_required
 def get_past_calc_result():
@@ -418,11 +521,12 @@ def get_past_calc_result():
 
     all_users_recipe = Recipe.query.filter_by(user_id=current_user.id).all()
     recipe_data = recipe_schema.dump(all_users_recipe)
-    
+
     return render_template(
         'layers_all.html',
         data=recipe_data
     )
+
 
 def create_app():
     db.init_app(app)
--- models.py
+++ models.py
@@ -1,10 +1,11 @@
+from datetime import datetime
+
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, date
-from sqlalchemy.sql import func
-# from flask_wtf import Form 
-#from wtforms import StringField, BooleanField
-#from wtforms.validators import DataRequired
+
+# from flask_wtf import Form
+# from wtforms import StringField, BooleanField
+# from wtforms.validators import DataRequired
 db = SQLAlchemy()
 
 
@@ -13,16 +14,18 @@ class Recipe(db.Model):
     __tablename__ = 'recipe'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     feed = db.Column(db.String(100), nullable=False)
-    amount_entered = db.Column(db.Integer, nullable=False)
+    number_of_days_to_feed = db.Column(db.Integer, nullable=False)
     result = db.Column(db.Integer, nullable=False)
-    date_added =  db.Column(db.DateTime, default= datetime.now)
+    date_added = db.Column(db.DateTime, default=datetime.now)
     user_id = db.Column(db.Integer,
                         db.ForeignKey('users.id', ondelete='CASCADE'),
                         nullable=False)
     coop_id = db.Column(db.Integer,
                         db.ForeignKey('coops.id', ondelete='CASCADE'),
                         nullable=False)
-# revised class Recipe for all feed calculations  
+
+
+# revised class Recipe for all feed calculations
 class FeedRecipe(db.Model):
     __tablename__ = 'feedrecipe'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -30,19 +33,21 @@ class FeedRecipe(db.Model):
     days = db.Column(db.Integer, nullable=False)
     total_feed = db.Column(db.Integer, nullable=False)
     total_cost = db.Column(db.Integer, nullable=False)
-    
-    #Kondwani - make calculations nased on specific coop chosen
-    #Kondwani - update user profile, delete coop
-    #for cost and amount for individual ingredients, make result downloadable
-    
-    date_added =  db.Column(db.DateTime, default= datetime.now)
+
+    # Kondwani - make calculations nased on specific coop chosen
+    # Kondwani - update user profile, delete coop
+    # for cost and amount for individual ingredients, make result downloadable
+
+    date_added = db.Column(db.DateTime, default=datetime.now)
     user_id = db.Column(db.Integer,
                         db.ForeignKey('users.id', ondelete='CASCADE'),
                         nullable=False)
     coop_id = db.Column(db.Integer,
                         db.ForeignKey('coops.id', ondelete='CASCADE'),
                         nullable=False)
-# 
+
+
+#
 # from flask_wtf import FlaskForm
 # from wtforms import StringField
 # from wtforms.validators import DataRequired
@@ -67,7 +72,7 @@ class FeedRecipe(db.Model):
 #     total_cost = db.Column(db.Integer(), nullable=False)
 #    coop_id = db.Column(db.Integer,
 #    db.ForeignKey('coops.id', ondedelete='CASCADE'),
-            #  nullable=False)
+#  nullable=False)
 #
 #
 class Users(UserMixin, db.Model):
@@ -76,35 +81,34 @@ class Users(UserMixin, db.Model):
     username = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True)
-    date_added =  db.Column(db.DateTime, default= datetime.now)
+    date_added = db.Column(db.DateTime, default=datetime.now)
     phone = db.Column(db.Integer(), nullable=False)
-#     image = db.Column(LargeBinary, nullable = True)
-#    
+    #     image = db.Column(LargeBinary, nullable = True)
+    #
     recipe_object = db.relationship('Recipe', backref='users', lazy=True, uselist=True,
                                     cascade="all,delete")
-    
-    coops_object = db.relationship('Coops', backref='users', lazy=True, uselist=True,
-                                     cascade="all,delete")
 
-    #class UsersForm(FlaskForm): for form validation and etc
-        
+    coops_object = db.relationship('Coops', backref='users', lazy=True, uselist=True,
+                                   cascade="all,delete")
+
+    # class UsersForm(FlaskForm): for form validation and etc
+
+
 class Coops(db.Model):
-    __tablename__= 'coops'
+    __tablename__ = 'coops'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
     age = db.Column(db.Integer, nullable=False)
     breed = db.Column(db.String(100), nullable=False)
-    number = db.Column(db.Integer, nullable=False)
-    date_added =  db.Column(db.DateTime, default= datetime.now)
+    number_of_chicks = db.Column(db.Integer, nullable=False)
+    date_for_next_feed = db.Column(db.DateTime, nullable=True)
+    date_added = db.Column(db.DateTime, default=datetime.now)
     user_id = db.Column(db.Integer,
-                         db.ForeignKey('users.id', ondelete='CASCADE'),
+                        db.ForeignKey('users.id', ondelete='CASCADE'),
                         nullable=False)
     recipe_object = db.relationship('Recipe', backref='coops', lazy=True, uselist=True,
                                     cascade="all,delete")
-    
-    
-    #date
-    
-   #age= (date_added-current_date)/7 + age
-    
-  
+
+    # date
+
+# age= (date_added-current_date)/7 + age
